8f6816a9763d194496813b3fd8e68f78110cbad4
tin <tin.pt@ntnc.vn>
2023-06-18 19:53:10 +0700

    Initial Commit

---
+++ Flash ChatUITests/Flash_ChatUITestsLaunchTests.swift
@@ -0,0 +1,32 @@
+//
+//  Flash_ChatUITestsLaunchTests.swift
+//  Flash ChatUITests
+//
+//  Created by Administrator on 18/06/2023.
+//
+
+import XCTest
+
+final class Flash_ChatUITestsLaunchTests: XCTestCase {
+
+    override class var runsForEachTargetApplicationUIConfiguration: Bool {
+        true
+    }
+
+    override func setUpWithError() throws {
+        continueAfterFailure = false
+    }
+
+    func testLaunch() throws {
+        let app = XCUIApplication()
+        app.launch()
+
+        // Insert steps here to perform after app launch but before taking a screenshot,
+        // such as logging into a test account or navigating somewhere in the app
+
+        let attachment = XCTAttachment(screenshot: app.screenshot())
+        attachment.name = "Launch Screen"
+        attachment.lifetime = .keepAlways
+        add(attachment)
+    }
+}
